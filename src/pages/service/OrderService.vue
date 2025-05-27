f2be2c82dfd6e7e30b48e55854e76c6406b31e5c
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-05-28 00:47:11 +0530

    add microservice dashborad

--- src/pages/service/OrderService.vue
+++ src/pages/service/OrderService.vue
@@ -1,3 +1,36 @@
 <template>
-    order-service
+    <div class="flex flex-col justify-center items-center mx-8 mt-4">
+        <div class="flex justify-center items-center w-full space-x-10">
+            <div class="w-1/2">
+                <ApiDetailCard
+                    service="Order Microservice"
+                    :status="true"
+                    uptime="23h 20m"
+                    :dependencies="[
+                        { name: 'DB', status: 'UP', latency: 10 },
+                        { name: 'Redis', status: 'UP', latency: 3 },
+                        { name: 'External API', status: 'UP', latency: 150 }
+                    ]"
+                    :cpuUsage="15"
+                    :memory="310"
+                    diskFree="4GB"
+                    :health="90"
+                    detailsLink="/services/auth"
+                    />
+
+            </div>
+            <div class="w-1/2">
+                <ApiUsageChart
+                    service="Order Service"
+                    :labels="['2025-05-20', '2025-05-21', '2025-05-22', '2025-05-23']"
+                    :apiUsageData="[140, 160, 155, 180]"
+                    class=""
+                />
+            </div>
+        </div>
+    </div>
 </template>
+<script setup>
+import ApiDetailCard from '../../components/Dashbord-UI-Card/ApiDetailCard.vue';
+import ApiUsageChart from '../../components/Dashbord-UI-Card/ApiUsageCard.vue';
+</script>
