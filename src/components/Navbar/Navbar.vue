9e289c871b8893d0d71556abed4a001288413a6d
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-10-31 14:30:46 +0530

    fixed navbar responsive issue

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="flex flex-wrap justify-between items-center p-2 px-4 bg-white shadow-md">
-    <p class="font-extrabold ml-16 text-lg sm:text-xl">
-      Stormgate Microservices
-    </p>
+  <div class="flex flex-wrap justify-between items-center p-2 px-4 bg-white shadow-md w-full">
+    <div class="flex items-center">
+      <p class="font-extrabold text-base sm:text-lg md:text-xl whitespace-nowrap">
+        Stormgate Microservices
+      </p>
+    </div>
 
-    <div class="flex items-center space-x-4 sm:space-x-6 md:space-x-7 mt-2 sm:mt-0">
+    <div class="flex items-center space-x-2 sm:space-x-4 md:space-x-6 mt-2 sm:mt-0">
       <div class="flex-col items-start hidden lg:flex">
         <p class="text-xs font-bold text-gray-500">
           Welcome
@@ -14,11 +16,11 @@
         </p>
       </div>
 
-      <div class="flex items-center bg-gray-50 shadow-lg rounded-lg p-2 hover:bg-gray-200 cursor-pointer">
+      <div class="flex items-center bg-gray-50 shadow-lg rounded-lg p-1.5 sm:p-2 hover:bg-gray-200 cursor-pointer">
         <Icon icon="mdi:bell-notification-outline" class="text-xl sm:text-2xl" />
       </div>
 
-      <div class="flex items-center rounded-full overflow-hidden w-10 h-10 sm:w-12 sm:h-12 border">
+      <div class="flex items-center rounded-full overflow-hidden w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 border">
         <img 
           src="../../assets/LogoBlack.png"
           class="transform transition ease-in-out duration-300 hover:scale-110"
