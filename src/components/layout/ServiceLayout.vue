455da23cdd763db224e7d3a94bda7cef47070adb
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-10-31 17:37:40 +0530

    changed the service layout

--- src/components/layout/ServiceLayout.vue
+++ src/components/layout/ServiceLayout.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex flex-col justify-center items-start w-11/12 ml-20 p-4">
+  <div class="flex flex-col justify-center items-start w-full px-4 sm:px-6 ml-6 lg:ml-8 lg:w-11/12">
     <p class="text-2xl font-bold mb-4">{{ serviceName }}</p>
 
-    <div class="flex w-full">
-      <div class="flex items-center justify-center border-b border-gray-200 pb-4 mb-4 w-full md:w-1/3">
+    <div class="w-full overflow-x-auto">
+      <div class="flex items-center justify-start sm:justify-center border-b border-gray-200 pb-4 mb-4 w-full min-w-max sm:min-w-0">
         <div
           v-for="(tab, index) in tabs"
           :key="index"
@@ -18,14 +18,14 @@
           <div class="m-2">
             <Icon :icon="tab.icon" />
           </div>
-          <p class="text-lg hidden sm:block">
+          <p class="text-sm sm:text-lg whitespace-nowrap">
             {{ tab.name }}
           </p>
         </div>
       </div>
     </div>
 
-    <div class="flex p-4 rounded-xl w-full">
+    <div class="flex p-2 sm:p-4 rounded-xl w-full overflow-x-auto">
       <Performance 
         v-if="activeTab === 0" 
         :serviceName="serviceName"
